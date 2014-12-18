55cd394282fc64eb08ba9c7e252f76e0d755b121
Jarrett Meyer <jarrettmeyer@gmail.com>
2014-12-18 16:24:48 -0500

    Added comments and cleaned up files.

--- demo01.js
+++ demo01.js
@@ -4,48 +4,54 @@ var uuid = require('node-uuid');
 var q = require('q');
 
 var data = {
-    'alice': { id: uuid.v4(), firstName: 'Alice' },
-    'bob': { id: uuid.v4(), firstName: 'Bob' },
-    'charlie': { id: uuid.v4(), firstName: 'Charlie' },
-    'diane': { id: uuid.v4(), firstName: 'Diane' }
+  'alice': { id: uuid.v4(), firstName: 'Alice' },
+  'bob': { id: uuid.v4(), firstName: 'Bob' },
+  'charlie': { id: uuid.v4(), firstName: 'Charlie' },
+  'diane': { id: uuid.v4(), firstName: 'Diane' }
 };
 
 function exists(key, callback) {
-    return setImmediate(function () {
-        return callback(!!data[key]);
-    });
+  return setImmediate(function () {
+    return callback(!!data[key]);
+  });
 }
 
 function getData(key, callback) {
-    var row = data[key];
-    if (row) {
-        return setImmediate(function () {
-            return callback(null, row);
-        });
-    } else {
-        return setImmediate(function () {
-            var err = 'Key not found: ' + key;
-            return callback(err, null);
-        });
-    }
+  var row = data[key];
+  if (row) {
+    return setImmediate(function () {
+      return callback(null, row);
+    });
+  } else {
+    return setImmediate(function () {
+      var err = 'Key not found: ' + key;
+        return callback(err, null);
+    });
+  }
 }
 
+/* The q.nfcall function converts a function with a callback argument into a
+    promise. The promise uses then and catch for results and errors,
+    respectively. */
 q.nfcall(getData, 'bob').then(function (result) {
-    console.log('nfcall getData bob:', result);
+  console.log('nfcall getData bob:', result);
 });
 
 q.nfcall(getData, 'mike').catch(function (error) {
-    console.log('nfcall getData mike:', error);
+  console.log('nfcall getData mike:', error);
 });
 
 q.nfcall(getData, 'diane').then(function (error) {
-    console.log('nfcall getData diane:', error);
+  console.log('nfcall getData diane:', error);
 });
 
 q.nfcall(getData, 'jack').catch(function (error) {
-    console.log('nfcall getData mike:', error);
+  console.log('nfcall getData mike:', error);
 });
 
+/* The callback for exists only accepts a single argument with the result.
+    not multiple arguments (e.g. error, result). This means that we cannot use
+    q.nfcall for this function to create a promise. */
 q.call(exists, 'alice').then(function (res) {
-    console.log('call exists alice:', res);
+  console.log('call exists alice:', res);
 });
--- demo02.js
+++ demo02.js
@@ -2,39 +2,41 @@ var q = require('q');
 
 
 function add(a, b) {
-    var args = Array.prototype.splice.call(arguments, 0);
-    var sum = 0;
-    args.forEach(function (arg) {
-        sum += arg;
-    });
-    return sum;
+  var args = Array.prototype.splice.call(arguments, 0);
+  var sum = 0;
+  args.forEach(function (arg) {
+    sum += arg;
+  });
+  return sum;
 }
 
 
 function delayedAdd(a, b, callback) {
-    var args = Array.prototype.splice.call(arguments, 0);
-    var callback = args.pop();
-    var sum = 0;
-    if (args.length === 0) {
-        return callback('No values to add.');
-    }
+  var args = Array.prototype.splice.call(arguments, 0);
+  callback = args.pop();
+  var sum = 0;
+  if (args.length === 0) {
+    return callback('No values to add.');
+  }
+  return setImmediate(function () {
     args.forEach(function (arg) {
         sum += arg;
     });
     return callback(null, sum);
+  });
 }
 
 /* Use nfcall to turn any node callback-based function into a promise. */
 q.nfcall(delayedAdd, 1, 2, 3).then(function (sum) {
-    console.log('sum using nfcall:', sum);
+  console.log('sum using nfcall:', sum);
 });
 
 /* nfcall can also catch errors when the error is returned as the first argument. */
 q.nfcall(delayedAdd).catch(function (error) {
-    console.log('error using nfcall:', error);
+  console.log('error using nfcall:', error);
 });
 
 /* fcall turns any function into a promise. */
 q.fcall(add, 1, 2, 3).then(function (sum) {
-    console.log('sum using fcall:', sum);
+  console.log('sum using fcall:', sum);
 });
